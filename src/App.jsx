3a4fa24c3a8b4f51d1e392c1fc01473a0e70493a
Brendan Conard <117928315+Brendanthatman@users.noreply.github.com>
2025-03-31 18:30:55 +0000

    started weather api

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,47 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Weather() {
+  let info = [];
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+      'X-RapidAPI-Key': 'ab6c56b840msh00dd6e776e7e18ep13c302jsncbe8de164cb2'
+    }
+  };
+  
+  async function getWeather(){
+    await fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=43.119%2C-85.559', options)
+    .then(response => response.json())
+      .then((response) => {
+        info.push(response.current.feelslike_f + '°F');
+        info.push(response.location.localtime);
+    })
+    .catch(err => console.error(err));
+  }
+  getWeather();
+
+  const listWeather = info.map(data =>
+    <li>{data}</li>
+  );
+
+  return (
+    <div>
+      <ul>
+        {listWeather}
+      </ul>
+    </div>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>MSU Dashboard</h1>
+      <Weather />
     </>
   )
 }
