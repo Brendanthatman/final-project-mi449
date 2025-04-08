0a35dc25ce25a1f0e415cc9cf412f1c6c31ad6c4
Brendan Conard <117928315+Brendanthatman@users.noreply.github.com>
2025-04-08 20:20:13 +0000

    Added users table to supabase and added functionality to JSX top bar, before messing with other stuff

--- src/layout/topbar/topbar.jsx
+++ src/layout/topbar/topbar.jsx
@@ -3,14 +3,30 @@ import Weather from '../../components/weather-component/weather';
 import CurrentTime from '../../components/current-time-component/currenttime';
 
 
-function TopBar(){
-    const [user, setUser] = useState("[name]");
+function TopBar(userEmailComponent){
+    const [userArray, setUserArray] = useState([]);
+
+    async function getTodaysClasses (userEmail) {
+      let { data: Users, error } = await supabase
+        .from('Users')
+        .select("*")
+        .like('user_email', userEmail)
+      setUserArray(Users);
+    }
+
+    getTodaysClasses(userEmailComponent);
   
     return (
       <>
       <div>
-        <h1>Hi, {user}.</h1>
-        <h2>Welcome to your MSU Dashboard.</h2>
+        {
+            userArray.map(theUser => {
+              return(
+                <h1>Hi, {theUser.user_first_name}</h1>
+              );
+            })
+          }
+          <h2>Welcome to your MSU Dashboard.</h2>
       </div>
       <Weather />
       <CurrentTime />
