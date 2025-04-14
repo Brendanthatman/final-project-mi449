ff961c04ae832d984f6373ecd72651c6778e524d
peggy1008123 <71455962+peggy1008123@users.noreply.github.com>
2025-04-14 14:26:28 -0400

    adding css for url

--- src/components/quicklinks/quicklinks.jsx
+++ src/components/quicklinks/quicklinks.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import "./quicklinks.css";
 
 function QuickLinks () {
     return (
         <div className="quicklinks">
-            <h3>Quick Links</h3>
-            <ul>
-                <li><a href="https://d2l.msu.edu/d2l/home">D2L</a></li>
-                <li><a href="https://lib.msu.edu/">MSU Library</a></li>
+            <h3 className='urlTitle w-xs p-2'>Quick Links</h3>
+            <ul className='urlContent p-5 w-2xs text-base'>
+                <li className='pb-2'><a href="https://d2l.msu.edu/d2l/home">D2L</a></li>
+                <li className='pb-2'><a href="https://lib.msu.edu/">MSU Library</a></li>
                 <li><a href="https://student.msu.edu/splash.html">SIS</a></li>
             </ul>
         </div>
