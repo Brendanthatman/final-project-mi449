2f4955138e27437e6640d7889e491bcd39bbed75
peggy1008123 <71455962+peggy1008123@users.noreply.github.com>
2025-04-14 16:16:40 -0400

    quick link's style finished

--- src/components/quicklinks/quicklinks.jsx
+++ src/components/quicklinks/quicklinks.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import "./quicklinks.css";
+import "../../App.css";
+import linkIcon from '../../assets/linkicon.png';
 
 function QuickLinks () {
     return (
         <div className="quicklinks">
-            <h3 className='urlTitle w-xs p-2'>Quick Links</h3>
-            <ul className='urlContent p-5 w-2xs text-base'>
-                <li className='pb-2'><a href="https://d2l.msu.edu/d2l/home">D2L</a></li>
-                <li className='pb-2'><a href="https://lib.msu.edu/">MSU Library</a></li>
-                <li><a href="https://student.msu.edu/splash.html">SIS</a></li>
+            <h3 className='blockTitle w-50 p-2 '>Quick Links</h3>
+            <ul className='blockContent p-5 w-3xs text-base text-left'>
+                <li className='pb-2 flex items-start'>
+                    <img src={linkIcon} className="w-6 h-6 mr-1" alt="Link Icon" />
+                    <a href="https://d2l.msu.edu/d2l/home" target="_blank" rel="noopener noreferrer">D2L</a>
+                </li>
+                <li className='pb-2 flex items-start'>
+                    <img src={linkIcon} className="w-6 h-6 mr-1" alt="Link Icon" />
+                    <a href="https://lib.msu.edu/" target="_blank" rel="noopener noreferrer">MSU Library</a>
+                </li>
+                <li className='flex items-start'>
+                    <img src={linkIcon} className="w-6 h-6 mr-1" alt="Link Icon" />
+                    <a href="https://student.msu.edu/splash.html" target="_blank" rel="noopener noreferrer">SIS</a>
+                </li>
             </ul>
         </div>
     );
