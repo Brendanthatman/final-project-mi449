f0cc3f70e482bce6048740e990d13ea4637ca7aa
peggy1008123 <71455962+peggy1008123@users.noreply.github.com>
2025-04-25 17:42:15 -0400

    Theme btn bg color fixed

--- src/layout/sidebar/sidebar.jsx
+++ src/layout/sidebar/sidebar.jsx
@@ -5,6 +5,7 @@ import lightmodeicon from '../../assets/lightmodeicon.png';
 import darkmodeicon from '../../assets/darkmodeicon.png';
 import mapicon from '../../assets/mapicon.png';
 import homeicon from '../../assets/homeicon.png';
+import "./sidebar.css";
 
 function SideBar () {
     const [msuLogo, setMsuLogo] = useState(msulogolight);
@@ -24,19 +25,19 @@ function SideBar () {
     return(
         <div className="sidebar fixed left-0 top-0 h-screen w-25 transition-colors duration-200">
             <div className="flex flex-col h-full justify-between py-4">
-                <ul className="flex flex-col items-center space-y-6">
+                <ul className="flex flex-col items-center space-y-3">
                     <li><a href="https://msu.edu/" className="p-2 rounded-lg">
                         <img src={msuLogo} alt="MSU Logo"  />
                     </a></li>
                     <li><a href="" className="p-2 rounded-lg">
                         <img src={homeicon} alt="Home" />
                     </a></li>
-                    <li><a href="https://maps.app.goo.gl/bT3FFW5VWG4TSFow8" className="p-2 rounded-lg">
+                    <li><a href="https://maps.app.goo.gl/bT3FFW5VWG4TSFow8" className=" rounded-lg">
                         <img src={mapicon} alt="Map" />
                     </a></li>
                 </ul>
                 <div className="flex justify-center">
-                    <button onClick={toggleTheme} className="p-2 rounded-lg">
+                    <button onClick={toggleTheme} className="p-2 rounded-lg toggleBtn">
                         <img src={mode} alt="Theme toggle" />
                     </button>
                 </div>
